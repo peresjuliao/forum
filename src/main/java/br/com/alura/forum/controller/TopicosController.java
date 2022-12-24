c1449551933ed8b2283d3504c43dac2bf669d604
Peres Juliao <peresjuliao@gmail.com>
2022-12-24 01:10:10 -0300

    Padrao repository Topico

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,21 +1,24 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDto> listar() {
-        Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
+        List<Topico> topicos = topicoRepository.findAll();
 
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+        return TopicoDto.converter(topicos);
     }
 }
